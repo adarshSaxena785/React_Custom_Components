1f389fff51aafaa8caaa9c8a06850ebb70094b55
Adarsh saxena <adarshs@vconstruct.in>
2025-10-21 12:13:01 +0530

    -Install tailwind and shadcn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
     <>
       <div>
         Aukat banane padegi bhai ab
+        <Button className='p-3 ml-4' >Ho jye buddy  </Button>
       </div>
     </>
   )
